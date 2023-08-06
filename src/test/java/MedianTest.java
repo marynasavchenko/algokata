2cb975ff33c5d855e91fb629c9969e84b80df80e
marynasavchenko <msavchenko.abacuspro@gmail.com>
2023-08-06 15:36:40 +0200

    median

--- src/test/java/MedianTest.java
+++ src/test/java/MedianTest.java
@@ -9,18 +9,18 @@ class MedianTest {
 
     @Test
     void valueInTheMiddleWhenNumOfObservationsIsOdd() {
-        List<Double> doubles = Arrays.asList(12.5, 18.3, 11.2, 19.0, 22.1, 14.3, 16.2, 12.5, 17.8, 16.5, 12.5);
+        List<Double> dataset = Arrays.asList(12.5, 18.3, 11.2, 19.0, 22.1, 14.3, 16.2, 12.5, 17.8, 16.5, 12.5);
 
-        Double median = Median.getMedian(doubles);
+        Double median = Median.getMedian(dataset);
 
         assertEquals(16.2, median);
     }
 
     @Test
     void averageOfMiddleWhenNumOfObservationsIsEven() {
-        List<Double> doubles = Arrays.asList(12.5, 18.3, 11.2, 19.0, 14.3, 16.2, 12.5, 17.8, 16.5, 12.5);
+        List<Double> dataset = Arrays.asList(12.5, 18.3, 11.2, 19.0, 14.3, 16.2, 12.5, 17.8, 16.5, 12.5);
 
-        Double median = Median.getMedian(doubles);
+        Double median = Median.getMedian(dataset);
 
         assertEquals(15.25, median);
 
